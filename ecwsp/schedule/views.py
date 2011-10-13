d4e82afed04972b1ea370c4200d139cc1fcb2d4c
David Burke <david@burkesoftware.com>
2011-10-13 11:45:24 -0400

    Fixed Engrade sync bug

--- ecwsp/schedule/views.py
+++ ecwsp/schedule/views.py
@@ -90,7 +90,7 @@ def teacher_grade(request):
                     marking_period = form.cleaned_data['marking_period']
                     include_comments = form.cleaned_data['include_comments']
                     courses = teacher.course_set.filter(marking_period=marking_period, graded=True)
-                    sec_courses = teacher.secondary_course_set.filter(marking_period=marking_period, graded=True)
+                    sec_courses = teacher.secondary_teachers.filter(marking_period=marking_period, graded=True)
                     courses = courses | sec_courses
                     courses = courses.distinct()
                     es = EngradeSync()
