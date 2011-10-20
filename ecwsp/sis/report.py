1bd01b9b76d63dfa475c5853eaf44238182b95d1
John Milner <jmilner@cristoreyny.org>
2011-10-20 13:30:57 -0400

    Let transcript generation fall through to schedule app even if benchmark_grade enabled

--- ecwsp/sis/report.py
+++ ecwsp/sis/report.py
@@ -230,7 +230,8 @@ def pod_report_grade(template, options, students, format="odt", transcript=True,
     """
     
     # to do: stop being so lazy. eventually people will need to access both pod_report_grade and pod_benchmark_report_grade.
-    if ("ecwsp.benchmark_grade" in settings.INSTALLED_APPS and
+    if (not transcript and
+        "ecwsp.benchmark_grade" in settings.INSTALLED_APPS and
         str(Configuration.get_or_default("Benchmark-based grading", "False").value).lower() == "true"):
         from ecwsp.benchmark_grade.report import pod_benchmark_report_grade
         return pod_benchmark_report_grade(template, options, students, format, transcript, report_card)
