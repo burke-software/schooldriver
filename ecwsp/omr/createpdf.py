c82541b9f392e4d2fdcbea8e4d4b49c01fbfd26d
calli <calli@burkesoftware.com>
2011-11-14 20:08:15 -0500

    let's try this

--- ecwsp/omr/createpdf.py
+++ ecwsp/omr/createpdf.py
@@ -564,9 +564,10 @@ def barcodeBoxgroup():
     db_cursor = db.cursor()
     db_cursor.execute("SET @questionnaire_id =(SELECT qid from questionnaires where description = " + str(testid) + ")")
     db_cursor.execute("SET @page_id = (SELECT pid from pages where qid = @questionnaire_id order by pid DESC limit 1)")
-    db_cursor.execute("SET @pageidalt = ((SELECT pid from pages order by pid DESC limit 1) +1)")
-    db_cursor.execute("SET @pageid = (SELECT IFNULL (@page_id,@pageidalt))")  
-    db_cursor.execute("INSERT INTO boxgroupstype (btid,width,pid,varname,sortorder) values (5,7,@pageid,'barcode_@pageid',0)")
+    db_cursor.execute("SET @pageid = ((SELECT pid from pages order by pid DESC limit 1) +1)")
+    #db_cursor.execute("SET @pageid = (SELECT IFNULL (@page_id,@pageidalt))")  
+    db_cursor.execute("INSERT INTO boxgroupstype (btid,width,pid,varname,sortorder) values (5,7,@pageid,'barcode_" +
+                      str(page) + "',0)")
     db_cursor.execute("INSERT INTO boxes (tlx,tly,brx,bry,pid,bgid,value)" +
                       " values (210, 185, 1175, 450, @pageid,LAST_INSERT_ID(),"+
                       str(student_id[id]) + ")")
