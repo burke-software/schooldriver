c601399ab5bed738089c0d3e71e9595da3c45506
calli <calli@burkesoftware.com>
2012-01-10 21:42:47 -0500

    updated the quexf trigger to pull by filled because QueXF is stupid. Needs Verified

--- general scripts/quexf_sword_sync.sql
+++ general scripts/quexf_sword_sync.sql
@@ -13,11 +13,35 @@ begin
             ON quexf_crny.boxgroupstype.bgid=quexf_crny.boxes.bgid 
             WHERE quexf_crny.boxes.bid = NEW.bid
     );
-    set @answer_id = (
-        SELECT DISTINCT boxes.value from quexf_crny.boxes JOIN quexf_crny.boxgroupstype
-            ON quexf_crny.boxes.bgid = quexf_crny.boxgroupstype.bgid
-            JOIN quexf_crny.formboxverifychar ON quexf_crny.boxes.bid=quexf_crny.formboxverifychar.bid
-            WHERE quexf_crny.formboxverifychar.bid = NEW.bid and quexf_crny.formboxverifychar.fid = NEW.fid
+    --Below doesn't work 10% of the time because queXF is stupid
+    --set @answer_id = (
+    --    SELECT DISTINCT boxes.value from quexf_crny.boxes JOIN quexf_crny.boxgroupstype
+    --        ON quexf_crny.boxes.bgid = quexf_crny.boxgroupstype.bgid
+    --        JOIN quexf_crny.formboxverifychar ON quexf_crny.boxes.bid=quexf_crny.formboxverifychar.bid
+    --        WHERE quexf_crny.formboxverifychar.bid = NEW.bid and quexf_crny.formboxverifychar.fid = NEW.fid
+    --);
+    --here is my replacement. I really hope this works....
+    DECLARE no_more_bids INT DEFAULT(0);
+    DECLARE bid_cursor CURSOR FOR SELECT quexf_crny.boxes.bid from quexf_crny.boxes WHERE quexf_crny.boxes.bgid =
+        (SELECT quexf_crny.boxes.bgid from quexf_crny.boxes WHERE quexf_crny.boxes.bid = NEW.bid
+    );
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET no_more_bids = 1;
+    set @answer_fill = 1.0;
+    OPEN bid_cursor;
+    FETCH bid_cursor INTO current_bid;
+    set @answer_bid = current_bid;
+    REPEAT
+        IF (SELECT quexf_crny.boxes.filled as current_fill from quexf_crny.boxes where quexf_crny.boxes.bid = current_bid) <
+            @answer_fill THEN
+            set @answer_fill = current_fill and set @answer_bid = current_bid;
+        END IF
+        FETCH bid_cursor INTO current_bid;
+    UNTIL no_more_bids = 1
+    END REPEAT;
+    close bid_cursor;
+
+    set @answer_id =(
+        SELECT DISTINCT boxes.value from quexf_crny.boxes WHERE boxes.bid = @answer_bid
     );
     set @points_earned = (
         SELECT crny.omr_answer.point_value from crny.omr_answer where crny.omr_answer.id =
@@ -45,4 +69,7 @@ begin
 end;
 end if;
 //
+
+--formboxes: bid, fid, filled
+--bgid
 
