c3ace5324bc51c872b3c5b7598d26afb1914b914
David Burke <david@burkesoftware.com>
2012-03-22 12:29:29 -0400

    Fixed issue where django would set up overridden permission with incorrect content type

---
+++ ecwsp/work_study/management/__init__.py
@@ -0,0 +1,18 @@
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.models import Permission
+from django.db.models.signals import post_syncdb
+from django.db.models import Q
+import ecwsp.work_study.models
+
+def fix_perm_content_type(sender, **kwargs):
+    work_user_content_type = ContentType.objects.get(app_label="work_study",name="work team user")
+    auth_content_type = ContentType.objects.filter(app_label="auth",name="user")
+    for perm in Permission.objects.filter(content_type=auth_content_type).filter(
+        Q(name="Can add work team user")
+        | Q(name="Can change work team user")
+        | Q(name="Can delete work team user")
+        ):
+        perm.content_type = work_user_content_type
+        perm.save()
+
+post_syncdb.connect(fix_perm_content_type, sender=ecwsp.work_study.models)
