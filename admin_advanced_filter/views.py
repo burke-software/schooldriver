91ac3af570be7b4748f7262baa43388ddcbdbb01
David Burke <david@burkesoftware.com>
2012-04-09 12:05:13 -0400

    more

--- admin_advanced_filter/views.py
+++ admin_advanced_filter/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.contrib.contenttypes.models import ContentType
+from django.template import RequestContext
 
-def show_filters(request, app, module):
+def get_filters(request, app, module):
     model_class = ContentType.objects.get(app_label=app, model=module).model_class()
-    
-    for field in model_class.model._meta.fields:
-        pass
+    fields = model_class._meta.fields
     
     return render_to_response(
-        'admin_advanced_filter/show_filters.html', 
-        {'msg': '',}, 
+        'admin_advanced_filter/get_filters.html', 
+        {'fields': fields,}, 
+        RequestContext(request, {}),
+    )
+
+def get_filter_field(request, ct, field_name):
+    model_class = ContentType.objects.get(id=ct).model_class()
+    return render_to_response(
+        'admin_advanced_filter/get_filter_field.html', 
+        {'fields': fields,}, 
         RequestContext(request, {}),
     )
