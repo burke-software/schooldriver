28e2160d08a306d70b9b6290aaeb90c2cdb26287
David Burke <david@burkesoftware.com>
2012-04-13 17:48:30 -0400

    f

--- ecwsp/work_study/models.py
+++ ecwsp/work_study/models.py
@@ -652,7 +652,8 @@ class TimeSheet(models.Model):
             send_mail(subject, msg, from_addr, [str(sendTo)])
         except:
             logging.warning('Could not email student', exc_info=True, extra={
-                'exception': sys.exc_info()[0]
+                'exception': sys.exc_info()[0],
+                'exception2': sys.exc_info()[1],
             })
         
     def save(self, *args, **kwargs):
