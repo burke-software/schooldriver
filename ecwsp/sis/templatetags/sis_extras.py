f6419f6fb3d4efcda059fbd214c84e59a8f9710b
David Burke <david@david-Gazelle-Professional.(none)>
2012-06-25 13:14:05 -0400

    Trying to fix sis_extra's to never error out

--- ecwsp/sis/templatetags/sis_extras.py
+++ ecwsp/sis/templatetags/sis_extras.py
@@ -1,37 +1,43 @@
 from django import template
-from django.template.defaultfilters import escape
 from django.utils.safestring import mark_safe
-from django.utils.safestring import SafeUnicode
-import os
 import re
 register = template.Library()
 
 @register.filter
 def in_group(user, group):
-	"""Returns True/False if the user is in the given group(s).
-	Usage::
-		{% if user|in_group:"Friends" %}
-		or
-		{% if user|in_group:"Friends,Enemies" %}
-		...
-		{% endif %}
-	You can specify a single group or comma-delimited list.
-	No white space allowed.
-	"""
-	import re
-	if re.search(',', group): group_list = re.sub('\s+','',group).split(',')
-	elif re.search(' ', group): group_list = group.split()
-	else: group_list = [group]
-	user_groups = []
-	for group in user.groups.all(): user_groups.append(str(group.name))
-	if filter(lambda x:x in user_groups, group_list): return True
-	else: return False
+    """Returns True/False if the user is in the given group(s).
+    Usage::
+        {% if user|in_group:"Friends" %}
+        or
+        {% if user|in_group:"Friends,Enemies" %}
+        ...
+        {% endif %}
+    You can specify a single group or comma-delimited list.
+    No white space allowed.
+    """
+    if re.search(',', group):
+        group_list = re.sub('\s+', '', group).split(',')
+    elif re.search(' ', group):
+        group_list = group.split()
+    else:
+        group_list = [group]
+    user_groups = []
+    try:
+        for group in user.groups.all():
+            user_groups.append(str(group.name))
+    except:
+        pass
+    if filter(lambda x:x in user_groups, group_list):
+        return True
+    else: return False
 in_group.is_safe = True
 
 
 class_re = re.compile(r'(?<=class=["\'])(.*)(?=["\'])')
 @register.filter
 def add_class(value, css_class):
+    """ Add a css class to the tag
+    """
     string = unicode(value)
     match = class_re.search(string)
     if match:
