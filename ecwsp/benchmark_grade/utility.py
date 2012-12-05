d511882ea9e92d7476b6505e86c046f21b447d4f
David Burke <dmbst32@gmail.com>
2012-12-05 18:04:22 -0500

    Update ecwsp/benchmark_grade/utility.py
    Fixed broken import

--- ecwsp/benchmark_grade/utility.py
+++ ecwsp/benchmark_grade/utility.py
@@ -16,7 +16,7 @@
 #   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #   MA 02110-1301, USA.
 
-from ecwsp.benchmark_grade.models import CalculationRule, Aggregate, Item, Mark
+from ecwsp.benchmark_grade.models import CalculationRule, Aggregate, Item, Mark, Category
 from ecwsp.schedule.models import MarkingPeriod
 from ecwsp.grades.models import Grade
 from django.db.models import Avg, Sum, Min
