6762876d9926293b34d008f9a0c54181b06241da
David Burke <david@david-dell-precision390.admin.cristoreyny.org>
2012-12-10 21:54:10 -0500

    Django 1.5 support No longer works with 1.4

--- admin_advanced_filter/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import *
-from views import *
-
-urlpatterns = patterns('',
-    (r'^get_filters/(?P<app>[a-z_]+)/(?P<module>[a-z_]+)$', get_filters),
-    (r'^get_filter_field$', get_filter_field),
-)
