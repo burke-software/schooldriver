7a41ae1b8ea9a2f0df468d8d96de459d68a6e56f
David Burke <dmbst32@gmail.com>
2013-02-02 18:47:59 -0500

    Update ecwsp/work_study/models.py
    Fixed #90
    Added no time sheet option to work study attendance

--- ecwsp/work_study/models.py
+++ ecwsp/work_study/models.py
@@ -776,7 +776,7 @@ class AttendanceReason(models.Model):
 class Attendance(models.Model):
     student = models.ForeignKey(StudentWorker, help_text="Student who is absent this day")
     absence_date = models.DateField(default=datetime.now, verbose_name="date")
-    tardy = models.CharField(verbose_name="Status", max_length=1, choices=(("P", "Present"),("A", "Absent/Half Day"),("T", "Tardy")),default="P")
+    tardy = models.CharField(verbose_name="Status", max_length=1, choices=(("P", "Present"),("A", "Absent/Half Day"),("T", "Tardy"),("N", "No Timesheet")),default="P")
     tardy_time_in = models.TimeField(blank=True,null=True)
     makeup_date = models.DateField(blank=True, null=True)
     fee = models.ForeignKey(AttendanceFee, blank=True, null=True)
