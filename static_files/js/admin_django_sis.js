b15abe0f715b5c4ee2df013ef94a46dfc23cedff
David Burke <dburke@e6500582u.admin.cristoreyny.org>
2013-03-21 12:58:26 -0400

    Cleaner solution for #110

--- static_files/js/admin_django_sis.js
+++ static_files/js/admin_django_sis.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
     $('a.menu-collapse-handler').click(function () {
-        $('li.grp-user-options-container.grp-open').removeClass('grp-open');
-        $('li.grp-user-options-container.grp-open').addClass('grp-closed');
+        var open = false;
         if ( $(this).parent().hasClass('grp-closed') ) {
+            open = true;
+        }
+        
+        $('li.grp-user-options-container').removeClass('grp-open');
+        $('li.grp-user-options-container').addClass('grp-closed');
+        
+        if ( open ) {
             $(this).parent().addClass('grp-open');
             $(this).parent().removeClass('grp-closed');
         } else {
