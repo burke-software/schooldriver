d3bc58c2c3bacad79a40ba13b87cb18f94b08062
David Burke <dmbst32@gmail.com>
2013-09-11 12:24:53 -0400

    I forgot this file and I'm an asshole

---
+++ ecwsp/attendance/dashboards.py
@@ -0,0 +1,58 @@
+from django.core.urlresolvers import reverse
+from responsive_dashboard.dashboard import Dashboard, Dashlet, ListDashlet, AdminListDashlet, LinksListDashlet
+from ecwsp.sis.dashboards import ReportBuilderDashlet
+from ecwsp.attendance.models import StudentAttendance
+from report_builder.models import Report
+
+import datetime
+
+class AttendanceDashlet(ListDashlet):
+    model = StudentAttendance
+    require_permissions = ('alumni.change_alumni',)
+    fields = ('student', 'date', 'status')
+    first_column_is_link = True
+    count = 15
+
+
+class AttendanceLinksListDashlet(LinksListDashlet):
+    links = [
+        {
+            'text': 'Take Homeroom Attendance',
+            'link': reverse('ecwsp.attendance.views.teacher_attendance'),
+            'perm': ('attendance.take_studentattendance'),
+        },
+        {
+            'text': 'Take Course Attendance',
+            'link': reverse('ecwsp.attendance.views.select_course_for_attendance'),
+            'perm': ('attendance.take_studentattendance'),
+        },
+        {
+            'text': 'Reports',
+            'link': reverse('ecwsp.attendance.views.attendance_report'),
+            'perm': ('studentattendance.reports'),
+        },
+    ]
+
+
+class AttendanceReportBuilderDashlet(ReportBuilderDashlet):
+    show_custom_link = '/admin/report_builder/report/?root_model__app_label=attendance'
+    custom_link_text = "Reports"
+    def _render(self, **kwargs):
+        self.queryset = Report.objects.filter(root_model__app_label='attendance')
+        # Show only starred when there are a lot of reports
+        if self.queryset.count() > self.count:
+            self.queryset = self.queryset.filter(starred=self.request.user)
+        return super(ReportBuilderDashlet, self)._render(**kwargs)
+
+
+class AttendanceDashboard(Dashboard):
+    app = 'attendance'
+    dashlets = [
+        AttendanceDashlet(title="Recent Attendance"),
+        AttendanceLinksListDashlet(title="Links"),
+        AttendanceReportBuilderDashlet(title="Reports",),
+        AdminListDashlet(title="Edit", app_label="attendance"),
+    ]
+
+
+dashboard = AttendanceDashboard()
