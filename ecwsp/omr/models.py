burke-software/schooldriver

commit 6d4c2eb4ca6ae9b79fc1362f6b35b44bf6dba34f

Added OMR teacher report

ecwsp/omr/models.py
@@ -63,6 +63,15 @@ class Test(models.Model):
         points_possible = self.testinstance_set.all()[0].points_possible
         return float(total_test_earned) / (total_tests_taken * points_possible)
     
+    def get_percent_scoring_over(self, min_score=70):
+        """ Calculate the percent of test takers scoring over the min_score
+            Defaults to 70.
+        """
+        total_test_earned =  self.testinstance_set.aggregate(total_earned=Sum('answerinstance__points_earned'))['total_earned']
+        total_tests_taken = self.testinstance_set.annotate(earned=Sum('answerinstance__points_earned')).filter(earned__gt=0).count()
+        points_possible = self.testinstance_set.all()[0].points_possible
+        return float(total_test_earned) / (total_tests_taken * points_possible)
+    
     
     def link_copy(self):
         from ecwsp.omr.views import test_copy
@@ -269,6 +278,13 @@ class AnswerAbstract(models.Model):
     def __unicode__(self):
         return strip_tags(self.answer)
     
+    @property
+    def letter(self):
+        if self.question.type == "True/False":
+            return self.answer[0]
+        else:
+            return chr(self.order + 65)
+    
 class AnswerBank(AnswerAbstract):
     question = models.ForeignKey(QuestionBank)
     
@@ -279,12 +295,6 @@ class Answer(AnswerAbstract):
     class Meta:
         ordering = ('order',)
 
-    @property
-    def letter(self):
-        if self.question.type == "True/False":
-            return self.answer[0]
-        else:
-            return chr(self.order + 65)
     
 class TestInstance(models.Model):
     student = models.ForeignKey('sis.Student')

ecwsp/omr/reports.py
@@ -142,11 +142,49 @@ class ReportManager(object):
         """ Make appy based report showing results for a whole class """
         report = TemplateReport()
         report.file_format = format
-        test_instances = test.testinstance_set.all()
-        benchmarks = Benchmark.objects.filter(question__test=test).distinct()
+        test_instances = test.testinstance_set.annotate(Sum('answerinstance__points_earned'))
+        test.benchmarks = Benchmark.objects.filter(question__test=test).distinct()
         
-        for benchmark in benchmarks:
-            benchmark.points_possible = test.question_set.filter(benchmarks=benchmark).aggregate(Sum('point_value'))['point_value__sum']
+        points_possible = test.points_possible
+        points_to_earn = 0.70 * test.points_possible
+        number_above_70 = test_instances.filter(answerinstance__points_earned__sum__gte=points_to_earn).count()
+        total_test_takers = test.testinstance_set.filter(answerinstance__points_earned__gt=0).distinct().count()
+        test.percent_over_70 = float(number_above_70) / total_test_takers
+
+        for benchmark in test.benchmarks:
+            question_benchmarks = test.question_set.filter(benchmarks=benchmark)
+            benchmark.points_possible = question_benchmarks.aggregate(Sum('point_value'))['point_value__sum']
+            benchmark.total_points_possible = benchmark.points_possible * test_instances.count()
+            benchmark.total_points_earned = question_benchmarks.aggregate(Sum('answerinstance__points_earned'))['answerinstance__points_earned__sum']
+            benchmark.average = float(benchmark.total_points_earned) / benchmark.total_points_possible 
+           
+            # Percent students over 70%
+            test_instances_over_70 = 0
+            for test_instance in test_instances:
+                 answers = test_instance.answerinstance_set.filter(question__benchmarks=benchmark)
+                 answers_points = answers.aggregate(Sum('points_earned'), Sum('points_possible'))
+                 instance_points_earned = answers_points['points_earned__sum']
+                 instance_points_possible = answers_points['points_possible__sum']
+                 instance_average = float(instance_points_earned) / instance_points_possible
+                 if instance_average >= 0.70:
+                     test_instances_over_70 += 1
+            benchmark.over_70 = float(test_instances_over_70) / test_instances.count()
+
+            benchmark.assessed_on = ""
+            for question_benchmark in question_benchmarks:
+                benchmark.assessed_on += "{}, ".format(question_benchmark.get_order_start_one)
+            benchmark.assessed_on = benchmark.assessed_on[:-2]
+        
+        test.questions = test.question_set.all()
+        for question in test.questions:
+            question.benchmark_text = ''
+            for benchmark in question.benchmarks.all():
+                question.benchmark_text += '{}, '.format(benchmark.number)
+            question.benchmark_text = question.benchmark_text[:-2]
+            question.num_correct = question.answerinstance_set.filter(points_earned__gte=F('points_possible')).count()
+            question.num_total = question.answerinstance_set.count()
+            question.percent_correct = float(question.num_correct) / question.num_total
+
             
         report.data['test'] = test
         report.data['tests'] = test_instances

ecwsp/omr/templates/omr/test_result.html
@@ -87,7 +87,7 @@
 	</a>
     </li>
     <li class="icon">
-	<a href="">
+	<a href="download_teacher_results">
 	  <img src="/static/omr/download_answer.jpg"/><br/>
 	  Teacher results (document)
 	</a>

ecwsp/omr/urls.py
@@ -28,6 +28,7 @@ urlpatterns = patterns('',
     (r'^test_result/(?P<test_id>\d+)/$', test_result),
     (r'^test_result/(?P<test_id>\d+)/download_xls/$', download_test_results),
     (r'^test_result/(?P<test_id>\d+)/download_student_results/$', download_student_results),
+    (r'^test_result/(?P<test_id>\d+)/download_teacher_results/$', download_teacher_results),
     (r'^test_result/(?P<test_id>\d+)/download_answer_sheets/$', download_answer_sheets),
     (r'^generatexml/(?P<test_id>\d+)', queXF_answer_sheets),
     (r'^question_bank/$', QuestionBankListView.as_view(

ecwsp/omr/views.py
@@ -466,6 +466,16 @@ def download_student_results(request, test_id):
     if not template:
         return HttpResponseRedirect(request.META.get('HTTP_REFERER','/'))
     return report.download_student_results(test, format, template)
+
+
+@user_passes_test(lambda u: u.has_perm("omr.teacher_test") or u.has_perm("omr.view_test") or u.has_perm("omr.change_test"))
+def download_teacher_results(request, test_id):
+    test = get_object_or_404(Test, id=test_id)
+    format = UserPreference.objects.get_or_create(user=request.user)[0].get_format(type="document")
+    template = Template.objects.get_or_create(name="OMR Teacher Test Result")[0].get_template(request)
+    if not template:
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER','/'))
+    return report.download_teacher_results(test, format, template)
     
     
 @user_passes_test(lambda u: u.has_perm("omr.teacher_test") or u.has_perm("omr.view_test") or u.has_perm("omr.change_test"))

ecwsp/sis/admin.py
@@ -189,11 +189,11 @@ class StudentAdmin(VersionAdmin, ReadPermissionModelAdmin, CustomFieldAdmin):
     change_list_template = "admin/sis/student/change_list.html"
     form = StudentForm
     readonly_fields = ['year']
-    search_fields = ['first_name', 'last_name', 'username', 'unique_id', 'street', 'state', 'zip', 'id']
+    search_fields = ['first_name', 'last_name', 'username', 'unique_id', 'street', 'state', 'zip', 'id', 'studentnumber__number']
     inlines = [StudentNumberInline, StudentCohortInline, StudentFileInline, StudentHealthRecordInline, TranscriptNoteInline, StudentAwardInline]
     actions = [mark_inactive]
     list_filter = ['is_active', 'year', 'class_of_year']
-    list_display = ['__unicode__','year']
+    list_display = ['first_name','last_name','year','is_active','primary_cohort', 'phone', 'gpa']
     if 'ecwsp.benchmark_grade' in settings.INSTALLED_APPS:
         filter_horizontal = ('family_access_users',)
         

commit 6732e5f410e52784ac0b98691e682d20324a7407

Remove broken and unused get_percent_scoring_over() method that was introduced by 6d4c2eb4

ecwsp/omr/models.py
@@ -79,16 +79,6 @@ class Test(models.Model):
         else:
             return None
     
-    def get_percent_scoring_over(self, min_score=70):
-        """ Calculate the percent of test takers scoring over the min_score
-            Defaults to 70.
-        """
-        total_test_earned =  self.active_testinstance_set.aggregate(total_earned=Sum('answerinstance__points_earned'))['total_earned']
-        total_tests_taken = self.active_testinstance_set.annotate(earned=Sum('answerinstance__points_earned')).filter(earned__gt=0).count()
-        points_possible = self.active_testinstance_set.all()[0].points_possible
-        return float(total_test_earned) / (total_tests_taken * points_possible)
-    
-    
     def link_copy(self):
         from ecwsp.omr.views import test_copy
         return '<a href="%s">Copy</a>' % (reverse(test_copy, args=[self.id]),)