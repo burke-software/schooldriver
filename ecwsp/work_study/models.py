0a4f85078d0cebc89a3a45847b8c04c6753a97cb
shirleyleu <shirley@burkesoftware.com>
2014-01-23 10:50:20 -0600

    Capitalisation fix

--- ecwsp/work_study/models.py
+++ ecwsp/work_study/models.py
@@ -266,7 +266,7 @@ class PaymentOption(models.Model):
 class StudentFunctionalResponsibility(models.Model):
     name = models.CharField(max_length=255)
     class Meta:
-        verbose_name_plural = "Student Functional Responsibilities"
+        verbose_name_plural = "Student functional responsibilities"
     def __unicode__(self):
         return unicode(self.name)
     
@@ -301,7 +301,7 @@ class CompContract(models.Model, CustomFieldModel):
     def __unicode__(self):
         return unicode(self.company)
     class Meta:
-        verbose_name = "Company Contract"
+        verbose_name = "Company contract"
     
     @property
     def get_payment_cost(self, strip=True):
