c0039264e120fca7ed57172a75e6f5a0a4b87d75
David Burke <dmbst32@gmail.com>
2014-03-25 12:44:40 -0400

    Using newer celery conf method Updated report builder version

--- django_sis/urls.py
+++ django_sis/urls.py
@@ -1,5 +1,5 @@
-from django.conf.urls import *
-from ecwsp.sis.views import *
+from django.conf.urls import include, patterns, url
+from ecwsp.sis import views as sis_views
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from django.contrib import admin
@@ -14,20 +14,20 @@ urlpatterns = patterns('',
     (r'^ckeditor/', include('ecwsp.ckeditor_urls')),# 1.6 compat  include('ckeditor.urls')),
     (r'^grappelli/', include('grappelli.urls')),
     (r'^$', 'ecwsp.sis.views.index'),
-
     (r'^sis/', include('ecwsp.sis.urls')),
     (r'^admin/jsi18n', 'django.views.i18n.javascript_catalog'),
     
+    (r'^report_builder/', include('report_builder.urls')),
+    (r'^simple_import/', include('simple_import.urls')),
     url(r'^accounts/password_change/$', 'django.contrib.auth.views.password_change'),
     url(r'^accounts/password_change_done/$', 'django.contrib.auth.views.password_change_done', name="password_change_done"),
-    (r'^logout/$', logout_view),
-  #  (r'^chart/$',chart_fte),
+    
+    (r'^logout/$', sis_views.logout_view),
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/jsi18n/$', 'django.views.i18n.javascript_catalog'),
     (r'^admin/', include(admin.site.urls) ),
-    
     (r'^ajax_select/', include('ajax_select.urls')),
-    url(dajaxice_config.dajaxice_url, include('ecwsp.dajaxice_urls')),#include('dajaxice.urls')),
+    url(dajaxice_config.dajaxice_url, include('ecwsp.dajaxice_urls')),
     (r'^reports/', include('scaffold_report.urls')),
     url(r"^su/", include("django_su.urls")),
 )
@@ -39,9 +39,6 @@ else:
 
 if 'ldap_groups' in settings.INSTALLED_APPS:
     urlpatterns += patterns('',(r'^ldap_grp/', include('ldap_groups.urls')),)
-if 'admin_advanced_filter' in settings.INSTALLED_APPS:
-    urlpatterns += patterns('', (r'^admin_advanced_filter/', include('admin_advanced_filter.urls')), )
-    
 if 'ecwsp.discipline' in settings.INSTALLED_APPS:
     urlpatterns += patterns('', (r'^discipline/', include('ecwsp.discipline.urls')), )
 if 'ecwsp.attendance' in settings.INSTALLED_APPS:
@@ -76,40 +73,13 @@ if 'rosetta' in settings.INSTALLED_APPS:
     urlpatterns += patterns('',
         url(r'^rosetta/', include('rosetta.urls')),
     )
-if 'report_builder' in settings.INSTALLED_APPS:
-    urlpatterns += url(r'^report_builder/', include('report_builder.urls')),
-if 'simple_import' in settings.INSTALLED_APPS:
-    urlpatterns += url(r'^simple_import/', include('simple_import.urls')),
-if 'responsive_dashboard' in settings.INSTALLED_APPS:
-    urlpatterns += url(r'^', include('responsive_dashboard.urls')),
-if 'sentry' in settings.INSTALLED_APPS:    
-    urlpatterns += patterns('', (r'^sentry/', include('sentry.web.urls')),)
 if 'social.apps.django_app.default' in settings.INSTALLED_APPS:    
     urlpatterns += patterns('', url('', include('social.apps.django_app.urls', namespace='social')),)
+
+urlpatterns += patterns('', (r'^', include('responsive_dashboard.urls')), )
 
 if settings.DEBUG:
     urlpatterns += patterns('',
         (r'^static/(.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
         (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT,})
     )
-
-if settings.CAS:
-    urlpatterns += patterns('',
-        (r'^accounts/login/$', 'cas.views.login'),
-        (r'^accounts/logout/$', 'cas.views.logout'),
-    )
-
-def handler500(request):
-    """
-    500 error handler which includes ``request`` in the context.
-
-    Templates: `500.html`
-    Context: None
-    """
-    from django.template import Context, loader
-    from django.http import HttpResponseServerError
-
-    t = loader.get_template('500.html') # You need to create a 500.html template.
-    return HttpResponseServerError(t.render(Context({
-        'request': request,
-    })))
