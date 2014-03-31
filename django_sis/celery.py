0fc0243e8541d4382f56fe0e29e5496fc2ead8bc
David Burke <dmbst32@gmail.com>
2014-03-30 20:21:27 -0400

    Fixed celery app name

--- django_sis/celery.py
+++ django_sis/celery.py
@@ -9,7 +9,7 @@ from django.conf import settings
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_sis.settings')
 
-app = Celery('django_sis_' + getattr(settings, 'SCHOOL_NAME', 'school'))
+app = Celery('django_sis')
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
