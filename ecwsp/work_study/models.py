645d60a1bf31633e2f70c10c180df2070f130655
David Burke <dmbst32@gmail.com>
2014-04-04 15:59:25 -0400

    CRA now references Faculty instead of User

--- ecwsp/work_study/models.py
+++ ecwsp/work_study/models.py
@@ -50,12 +50,12 @@ import logging
 from celery.contrib.methods import task
 
 from ecwsp.administration.models import Configuration, Template
-from ecwsp.sis.models import Student
+from ecwsp.sis.models import Student, Faculty
 from ecwsp.sis.helper_functions import CharNullField
 from ecwsp.sis.template_report import TemplateReport
 
 class CraContact(models.Model):
-    name = models.ForeignKey(User)
+    name = models.ForeignKey(Faculty)
     email = models.BooleanField(default=False, help_text="Receive daily e-mail listing all supervisor comments about student.")
     email_all = models.BooleanField(default=False, help_text="Receive comments about all students.")
     def __unicode__(self):
@@ -510,6 +510,7 @@ class StudentWorker(Student):
         try: return self.placement.pickup_location
         except: return ""
     
+    @property
     def cra(self):
         try: return self.placement.cra
         except: return ""
