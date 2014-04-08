99e7ece5d404f952dd21d2db09f36fee4b4a186e
root <root@newsword.cristoreyny.org>
2014-04-07 20:46:28 -0400

    Fixed sentry issue 17666209 grades.admin misconfigured

--- ecwsp/grades/admin.py
+++ ecwsp/grades/admin.py
@@ -7,5 +7,5 @@ admin.site.register(GradeComment)
 class GradeAdmin(admin.ModelAdmin):
     list_display = ['grade', 'course', 'student', 'marking_period', 'override_final']
     list_filter = ['date', 'override_final']
-    search_fields = ['student__fname', 'student__lname', 'course__fullname', 'course__shortname']
+    search_fields = ['student__first_name', 'student__last_name', 'course__fullname', 'course__shortname']
 admin.site.register(Grade, GradeAdmin)
