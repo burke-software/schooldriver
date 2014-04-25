d818d788ebf9425ebbc4d30e957f4e7e8ad4f90d
qdonnellan <qdonnellan@gmail.com>
2014-04-25 18:28:40 -0500

    only admins should be able to see grades

--- api/grades/views.py
+++ api/grades/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
 from ecwsp.grades.models import Grade
 from api.grades.serializers import GradeSerializer
 
@@ -6,5 +7,6 @@ class GradeViewSet(viewsets.ModelViewSet):
     """
     an API endpoint for the Grade model
     """
+    permission_classes = (IsAdminUser,)
     queryset = Grade.objects.all()
     serializer_class = GradeSerializer
