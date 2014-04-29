e90a5763ed3ab824a3b51c703c8df42890c20b15
michellejaeg <michelles82@gmail.com>
2014-04-28 18:30:08 -0700

    grade calculation unit test

--- ecwsp/grades/tests.py
+++ ecwsp/grades/tests.py
@@ -1,16 +1,23 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
+from ecwsp.sis.models import *
 from django.test import TestCase
+import datetime
+from models import *
+import random
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class GradeCalculationTests(TestCase):
+
+    def setUp(self):
+        self.student = Student.objects.create(username='ttesty')
+
+    def test_current_vs_older(self):
+        StudentYearGrade.objects.create(student=self.student, year_name='2011-2012')
+        for i in range(20):
+            grade = random.random()
+            a_grade = Grade.objects.create(grade=grade)
+            self.student.studentyeargrade_set.add(a_grade)
+        syg = self.student.studentyeargrade_set.get(year_name='2011-2012')
+        current = syg.calculate_grade(date_report=datetime.date.today())
+        older = syg.calculate_grade()
+        self.assertEqual(current, older)
+
