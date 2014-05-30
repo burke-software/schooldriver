107db7a96be072d875250f5d33913177d255237d
qdonnellan <qdonnellan@gmail.com>
2014-05-30 13:25:20 -0500

    some work with nested serializers

--- api/schedule/serializers.py
+++ api/schedule/serializers.py
@@ -1,5 +1,26 @@
-from ecwsp.schedule.models import Course, CourseSection
+from ecwsp.schedule.models import Course, CourseSection, Department
 from rest_framework import serializers
+
+class DepartmentNestedSerializer(serializers.ModelSerializer):
+    """
+    a department serializer for nesting purposes
+    should be passed "read_only=True"
+    """
+
+    class Meta:
+        model = Department
+        fields = ('id', 'name',)
+
+class CourseNestedSerializer(serializers.ModelSerializer):
+    """
+    a course serializer for nesting purposes
+    should be passed "read_only=True"
+    """
+    department = DepartmentNestedSerializer(source='department', read_only=True) 
+
+    class Meta:
+        model = Course
+        fields = ('id', 'fullname', 'credits', 'graded', 'department',)
 
 class CourseSerializer(serializers.ModelSerializer):
     """
@@ -8,6 +29,8 @@ class CourseSerializer(serializers.ModelSerializer):
 
     id = serializers.Field()
     sections = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    department_id = serializers.PrimaryKeyRelatedField(source='department')
+    department = DepartmentNestedSerializer(source='department', read_only=True)
 
     class Meta:
         model = Course
@@ -18,6 +41,13 @@ class SectionSerializer(serializers.ModelSerializer):
     """
 
     id = serializers.Field()
+    course_id = serializers.PrimaryKeyRelatedField(source='course')
+    course = CourseNestedSerializer(source='course', read_only=True)
 
     class Meta:
         model = CourseSection
+
+
+
+
+
