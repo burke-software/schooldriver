6fb741b70f324dfb010ff4f2388c9dbf8dc0231c
David Burke <dmbst32@gmail.com>
2014-06-11 15:38:02 -0400

    Allow blank grade field in grades api [12289491]

--- api/grades/serializers.py
+++ api/grades/serializers.py
@@ -13,7 +13,7 @@ class GradeSerializer(serializers.ModelSerializer):
     marking_period_id = serializers.PrimaryKeyRelatedField(source='marking_period', required=False)
     course_section = serializers.RelatedField(source='course_section', read_only=True)
     course_section_id = serializers.PrimaryKeyRelatedField(source='course_section', required=False)
-    grade = serializers.WritableField(source='api_grade')
+    grade = serializers.WritableField(source='api_grade', required=False)
 
     course = serializers.RelatedField(source='course_section', read_only=True)
     course_id = serializers.PrimaryKeyRelatedField(source='course_section', required=False)
@@ -21,7 +21,7 @@ class GradeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Grade
-        
+
     def validate_grade(self, attrs, source):
         value = attrs[source]
         Grade.validate_grade(value)
