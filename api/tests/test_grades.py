3f44bd2cac79fb05a92ba759235cbc16952a68db
qdonnellan <qdonnellan@gmail.com>
2014-06-20 11:43:16 -0500

    updated api grades test

--- api/tests/test_grades.py
+++ api/tests/test_grades.py
@@ -16,38 +16,13 @@ class GradeAPITest(APITest):
         response = self.client.get('/api/grades/')
         self.assertEqual(response.status_code, 403)
 
-        # try it with a normal user (authenticated but wrong permissions)
-        self.client.force_authenticate(user=self.user)
+        # try it with teacher authentication
+        self.client.force_authenticate(user = self.data.teacher1)
+        response = self.client.get('/api/grades/')
+        self.assertEqual(response.status_code, 200)
+
+        # try it with a student (who clearly has no authoritay)
+        self.client.force_authenticate(user = self.data.student)
         response = self.client.get('/api/grades/')
         self.assertEqual(response.status_code, 403)
 
-        # sign out...
-        self.client.force_authenticate(user=None)
-
-        # try it with an admin user (authenticated)
-        self.client.force_authenticate(user=self.admin_user)
-        response = self.client.get('/api/grades/')
-        self.assertEqual(response.status_code, 200)
-
-    def test_simple_post_request(self):
-        """
-        test a post request for a student's grade using the api
-        """
-        # make sure no grades currently exist!
-        self.client.force_authenticate(user=self.admin_user)
-        response = self.client.get('/api/grades/')
-        number_of_grades = len(json.loads(response.content))
-        self.assertEqual(number_of_grades, 0)
-
-        # create a new grade
-        enrollment = self.enroll_student_in_test_section()
-        data = {
-            'student_id': enrollment.user.id,
-            'course' : enrollment.section.id,
-            'marking_period' : enrollment.section.marking_period.id,
-            'grade' : 95,
-            'override_final' : False,
-            'comment' : 'a job well done',
-        }
-        response = self.client.post('/api/grades/', data)
-        self.assertEqual(response.status_code, 200)
