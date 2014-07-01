679c4c59d4ab32064101de1d909e12dac93e9b8c
qdonnellan <quentin@burkesoftware.com>
2014-07-01 14:59:54 -0500

    removed some permissions stuff

--- api/grades/views.py
+++ api/grades/views.py
@@ -1,7 +1,5 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
-from api.permissions import BelongsToStudent
-from api.filters import BelongsToStudentFilter
+from rest_framework.permissions import IsAdminUser
 from rest_framework import filters
 from ecwsp.grades.models import Grade
 from ecwsp.schedule.models import CourseSection
@@ -12,10 +10,9 @@ class GradeViewSet(viewsets.ModelViewSet):
     """
     an API endpoint for the Grade model
     """
-    permission_classes = (IsAuthenticated, BelongsToStudent)
+    permission_classes = (IsAdminUser,)
     queryset = Grade.objects.all()
     filter_backends = (filters.DjangoFilterBackend, filters.OrderingFilter)
-    #filter_backends = (BelongsToStudentFilter,)
     serializer_class = GradeSerializer
     filter_fields = ('student', 'course_section', 'course_section__marking_period__school_year')
     ordering_fields = ('marking_period__start_date',)
