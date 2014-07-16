59abc6e9ea10452f810a10a6babb8113acea507e
David Burke <dmbst32@gmail.com>
2014-07-16 14:20:10 -0400

    Collect fast to make heroku suck less

--- django_sis/settings.py
+++ django_sis/settings.py
@@ -415,7 +415,8 @@ if 'social.apps.django_app.default' in INSTALLED_APPS:
 if 'ON_HEROKU' in os.environ:
     ON_HEROKU = True
     # Use S3
-    INSTALLED_APPS += ('storages',)
+    INSTALLED_APPS += ('storages', 'collectfast')
+    AWS_PRELOAD_METADATA = True
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
     STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
     for environment_variable in (
