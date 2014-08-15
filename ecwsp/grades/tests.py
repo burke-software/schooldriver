e426f6004fac464b9d37a08bb5ff06b8ccd62224
David Burke <dmbst32@gmail.com>
2014-08-14 22:58:03 -0400

    Added letter grade cache test [12917068]

--- ecwsp/grades/tests.py
+++ ecwsp/grades/tests.py
@@ -326,6 +326,22 @@ class GradeBaltTests(SisTestMixin, TestCase):
         self.assertAlmostEqual(gpa, Decimal(3.6))
 
 
+class GradeCacheTests(SisTestMixin, TestCase):
+    def setUp(self):
+        super(GradeCacheTests, self).setUp()
+        self.build_grade_cache()
+
+    def test_passing_letter_grade(self):
+        student = self.data.student
+        section = self.data.course_section
+        grades = student.grade_set.filter(course_section=section)
+        for grade in grades:
+            grade.set_grade('HP')
+            grade.save()
+        ce = student.courseenrollment_set.filter(course_section=section).first()
+        self.assertEqual(ce.grade, 'P')
+
+
 class GradeScaleTests(SisTestMixin, TestCase):
     def setUp(self):
         super(GradeScaleTests, self).setUp()
