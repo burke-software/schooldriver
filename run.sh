4d09f05c630572e2d78aaef2cf50283b1d88f2b4
David Burke <dmbst32@gmail.com>
2014-09-12 19:40:55 -0400

    Supervisor now is running libreoffice

--- run.sh
+++ run.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 export C_FORCE_ROOT="true"
 python manage.py celery worker -E -A django_sis --loglevel=INFO &
-mkdir -p /tmp/django-sis_libreoffice
-export HOME=/tmp/django-sis_libreoffice
-/usr/lib/libreoffice/program/soffice.bin --accept='socket,host=localhost,port=2002;urp;StarOffice.ServiceManager' --headless &
-sleep 2 && /usr/lib/libreoffice/program/soffice.bin --accept='socket,host=localhost,port=2002;urp;StarOffice.ServiceManager' --headless &
+supervisord -c /etc/supervisor/supervisord.conf
 python manage.py runserver_plus 0.0.0.0:8000
 
 
