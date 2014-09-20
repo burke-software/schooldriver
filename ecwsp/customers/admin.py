b75afae82b5beb4041d9fce3e4c6d29cb340f132
David Burke <dmbst32@gmail.com>
2014-09-20 11:41:47 -0400

    Added admin to customers model

--- ecwsp/customers/admin.py
+++ ecwsp/customers/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Client
 
-# Register your models here.
+class ClientAdmin(admin.ModelAdmin):
+    model = Client
+    list_display = ('name', 'schema_name', 'on_trial', 'paid_until',)
+    search_fields = ('name', 'schema_name',)
+    list_filter = ('on_trial',)
+admin.site.register(Client, ClientAdmin)
