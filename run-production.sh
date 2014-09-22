6cdcd2e81f7310592d6da0d1a4dd31e877a03d13
David Burke <dmbst32@gmail.com>
2014-09-22 15:19:55 -0400

    Added new relic by default on production

--- run-production.sh
+++ run-production.sh
@@ -2,5 +2,5 @@
 # This is for running multiple commands via docker in production
 supervisord -c /etc/supervisor/supervisord.conf
 python manage.py celery flower &
-gunicorn ecwsp.wsgi --log-file - -b 0.0.0.0:8000 -n dev.schooldriver_org
+newrelic-admin run-program  gunicorn ecwsp.wsgi --log-file - -b 0.0.0.0:8000 -n dev.schooldriver_org
 
