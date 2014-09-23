3f1a885f6784186e6f1ea3e47c88acf0daff1ef8
qdonnellan <quentin@burkesoftware.com>
2014-09-23 10:25:43 -0500

    tests to go along with the new template model (still incomplete)

---
+++ ecwsp/admissions/tests/student_application_test.py
@@ -0,0 +1,30 @@
+from django.test import TestCase
+from ecwsp.admissions.models import StudentApplicationTemplate
+
+class TestStudentApplicationTemplateModel(TestCase):
+    """
+    test the implementation of our StudentApplicationTemplate
+    """
+    def create_simple_application_template(self):
+        """
+        create a simple application for use with the following tests
+        """
+        simple_template = {
+            'sections' : [
+                {'name': 'Personal Information'},
+                {'name': 'Contact Information'}
+            ]
+        }
+        new_application = StudentApplicationTemplate()
+        new_application.name = 'Simple1'
+        new_application.template = simple_template
+        new_application.save()
+
+    def test_creation_of_simple_template(self):
+        self.create_simple_application_template()
+        application = StudentApplicationTemplate.objects.get(name='Simple1')
+        json_template = application.template
+        sections = json_template['sections']
+        self.assertEqual(len(sections), 2)
+        self.assertEqual(sections[0]['name'], 'Personal Information')
+        
