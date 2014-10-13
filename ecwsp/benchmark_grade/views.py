burke-software/schooldriver

commit 80409ac292c55131dc15e69b3b181c30a23f4b61

Minor hack to make TC's reports easier

ecwsp/benchmark_grade/views.py
@@ -176,7 +176,9 @@ def gradebook(request, course_id, for_export=False):
     # whoa, super roll of the dice. is Item.demonstration_set really guaranteed to be ordered by id?
     # precarious; sorting must match items (and demonstrations!) exactly
     marks = Mark.objects.filter(item__in=items).order_by('item__id', 'demonstration__id').all() 
-    items_count = items.filter(demonstration=None).count() + Demonstration.objects.filter(item__in=items).count()
+    items_count = items.filter(demonstration=None).count() + \
+        items.exclude(demonstration=None).count() + \
+        Demonstration.objects.filter(item__in=items).count()
     for student in students:
         student_marks = marks.filter(student=student).select_related('item__category_id')
         student_marks_count = student_marks.count()
@@ -187,6 +189,8 @@ def gradebook(request, course_id, for_export=False):
                     # must create mark for each demonstration
                     for demonstration in item.demonstration_set.all():
                         mark, created = Mark.objects.get_or_create(item=item, demonstration=demonstration, student=student)
+                    # Create one extra with demonstration=None to store the aggregate of Demonstrations
+                    mark, created = Mark.objects.get_or_create(item=item, demonstration=None, student=student)
                 else:
                     # a regular item without demonstrations; make only one mark
                     mark, created = Mark.objects.get_or_create(item=item, student=student)

ecwsp/benchmark_grade/models.py
@@ -286,6 +286,26 @@ class Mark(models.Model):
             self.normalized_mark = float(self.mark) / float(self.item.points_possible)
         super(Mark, self).save(*args, **kwargs)
 
+        # For Categories that allow multiple Demonstrations, store the aggregate
+        # mark (attribute) in the Mark (model) that's linked to an Item but not
+        # to any Demonstration.
+        # This way, reports can be run using `demonstration=None` as a filter
+        # to include only one mark per Student per Assignment.
+        if self.demonstration is not None and \
+        self.item.category.allow_multiple_demonstrations:
+            Method = self.item.category.aggregation_method
+            aggregated_mark = self.item.mark_set.filter(
+                student=self.student
+            ).exclude(
+                demonstration=None
+            ).aggregate(Method('mark')).popitem()[1]
+            aggregated_model, created = benchmark_get_create_or_flush(
+                Mark, student=self.student, item=self.item, demonstration=None
+            )
+            aggregated_model.mark = aggregated_mark
+            aggregated_model.save()
+
+
     def clean(self):
         from django.core.exceptions import ValidationError
         if self.item.category.fixed_granularity and self.mark and self.mark % self.item.category.fixed_granularity != 0:

commit 1beaf3fecd46f3e47b33c9d589bf9608719f51cb

Fix incorrect 'Students Demonstrated' counts in gradebook, caused by 80409ac292c55131dc15e69b3b181c30a23f4b61 [14988003]

ecwsp/benchmark_grade/views.py
@@ -230,8 +230,12 @@ def gradebook(request, course_section_id, for_export=False):
                 if item.category != standards_category:
                     item.marks_counts = 'N/A'
                     continue
-                marks_count_passing = item.mark_set.filter(mark__gte=PASSING_GRADE).count()
-                marks_count_total = item.mark_set.exclude(mark=None).count()
+                if item.category.allow_multiple_demonstrations:
+                    mark_set = item.mark_set.exclude(demonstration=None)
+                else:
+                    mark_set = item.mark_set.filter(demonstration=None)
+                marks_count_passing = mark_set.filter(mark__gte=PASSING_GRADE).count()
+                marks_count_total = mark_set.exclude(mark=None).count()
                 if marks_count_total:
                     item.marks_counts = '{} / {} ({:.0f}%)'.format(marks_count_passing, marks_count_total, 100.0 * marks_count_passing / marks_count_total)
                 else: