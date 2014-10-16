e1758a4237a100b7f4f3617bea39c90ae3da30ee
qdonnellan <quentin@burkesoftware.com>
2014-10-15 20:38:56 -0500

    this seems to be necessary now... will have to take a look later

--- django_sis/settings.py
+++ django_sis/settings.py
@@ -547,3 +547,5 @@ REST_FRAMEWORK = {
     'TEST_REQUEST_DEFAULT_FORMAT': 'json',
     'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',)
 }
+
+ADMISSIONS_DEFAULT_COUNTRY = None
