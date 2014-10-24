4e3cf6d2ff9f110c8d51ab65cc3ae51d183e96c2
qdonnellan <quentin@burkesoftware.com>
2014-10-24 18:14:40 -0500

    updated API router

--- api/routers.py
+++ api/routers.py
@@ -1,9 +1,15 @@
 from rest_framework import routers
 from api.grades.views import GradeViewSet
 from api.schedule.views import CourseViewSet, SectionViewSet
+from api.admissions.views import ApplicantViewSet
+from api.admissions.views import ApplicantCustomFieldViewSet
+from api.admissions.views import ApplicationTemplateViewSet
 
 router = routers.DefaultRouter()
 router.register(r'grades', GradeViewSet)
 router.register(r'courses', CourseViewSet)
 router.register(r'sections', SectionViewSet)
+router.register(r'applicant', ApplicantViewSet)
+router.register(r'applicant-custom-field', ApplicantCustomFieldViewSet)
+router.register(r'application-template', ApplicationTemplateViewSet )
 api_urls = router.urls
