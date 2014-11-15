5d0bf8445d562b1db2eaaedd64fd3ca3e1bc14bf
qdonnellan <quentin@burkesoftware.com>
2014-11-14 22:34:34 -0600

    added an admin endpoint for syncing new edits to the application/custom fields with the app's .po file for translations

--- api/translations/views.py
+++ api/translations/views.py
@@ -1,8 +1,10 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
+from api.translations.sync import SyncApplicationTranslationFile
+from rest_framework.permissions import IsAdminUser
 import polib
 
-class TranslationViewSet(viewsets.ViewSet):
+class AdmissionsTranslationViewSet(viewsets.ViewSet):
     def list(self, request):
         lang = self.request.QUERY_PARAMS.get('lang', None)
         if lang is not None:
@@ -19,3 +21,19 @@ class TranslationViewSet(viewsets.ViewSet):
             return Response(
                 {"error": "need to specify a language with ?lang="}, 400
                 )
+
+class AdmissionsTranslationSync(viewsets.ViewSet):
+    """ sync the application data with the admissions' .po file,
+    useful after copy changes or additions to the application template or
+    application custom fields """
+
+    permission_classes = (IsAdminUser,)
+
+    def list(self, request):
+        try:
+            new_app_sync = SyncApplicationTranslationFile()
+            new_app_sync.sync()
+        except Exception as e:
+            return Response({"error": str(e)}, 500)
+        else:
+            return Response({"success" : "successful application translation sync"})
