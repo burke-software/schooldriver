9f4c47da4dc51fa8f68b79f5cda3a53bbc12fe7d
qdonnellan <quentin@burkesoftware.com>
2014-12-01 10:23:10 -0600

    removing functions that now live inside the application service - also removing unused variables

--- static_files/app/admissions/review-student-application.js
+++ static_files/app/admissions/review-student-application.js
@@ -1,54 +1,16 @@
 app.controller('ReviewStudentApplicationController', 
-    function($scope, $route, $routeParams, ApplicationFieldFactory, ApplicationTemplateFactory, Restangular) {
-        $scope.$routeParams = $routeParams;
-        $scope.submissionDate = "";
+    function($scope, $routeParams, Restangular, ApplicationService) {
 
-        // get this applicant's saved data
-        Restangular.one('applicant', $routeParams.applicantId).get()
-            .then( function(applicant) {
-                $scope.applicantData = applicant
-            });
-
-        // get the default application template and format it
-        ApplicationTemplateFactory.getList({is_default: 'true'})
-            .then( function(templates) {
-                var applicationTemplate = templates[0];
-                var jsonTemplate = JSON.parse(applicationTemplate.json_template)
-                $scope.applicationTemplate = jsonTemplate;
-            }).then( function() {
-                ApplicationFieldFactory.getList().then( function(fields) {
-                    $scope.applicationFields = fields;
-                }).then( function() {
-                    $scope.populateApplicationTemplateWithApplicationFields();
+        ApplicationService.loadDefaultTemplate().then( function() {
+            $scope.applicationTemplate = ApplicationService.defaultTemplate;
+            // get this applicant's saved data and populate template with it
+            Restangular.one('applicant', $routeParams.applicantId).get()
+                .then( function(applicant) {
+                    $scope.applicantData = applicant;
                 }).then( function() {
                     $scope.populateApplicationTemplateWithStudentResponses();
                 });
-            });
-
-        $scope.populateApplicationTemplateWithApplicationFields = function() {
-            // applicationt template simply contains field IDs - we need to 
-            // match those field id's with the corresponding field and inject
-            // that field into the template
-            var template_sections = $scope.applicationTemplate.sections;
-            for (var section_id in template_sections) {
-                var section = template_sections[section_id];
-                for (var field_id in section.fields) {
-                    var section_field = section.fields[field_id];
-                    var custom_field = $scope.getApplicationFieldById(section_field.id);
-                    section.fields[field_id] = custom_field;
-                }
-            }
-        };
-
-        $scope.getApplicationFieldById = function(field_id) {
-            for ( var i in $scope.applicationFields ) {
-                var field = $scope.applicationFields[i];
-                if ( field.id == field_id ) {
-                    return field;
-                    break;
-                }
-            }
-        };
+        });
 
         $scope.populateApplicationTemplateWithStudentResponses = function() {
             var template = $scope.applicationTemplate;
@@ -67,9 +29,9 @@ app.controller('ReviewStudentApplicationController',
         }; 
 
         $scope.getAdditionalFieldValue = function(field) {
-            var additionalData = $scope.getApplicationFieldById(field.id);
+            var additionalData = $scope.getAdditionalDataByFieldId(field.id);
             if ( field.field_type == 'emergency_contact' ) {
-                return $scope.convertPythonUnicodeToJsonObject(additionalData.answer); 
+                return ApplicationService.convertPythonUnicodeToJsonObject(additionalData.answer); 
             } else {
                 return additionalData.answer;
             }
@@ -88,12 +50,6 @@ app.controller('ReviewStudentApplicationController',
             return additional;
         };
 
-        $scope.convertPythonUnicodeToJsonObject = function(pythonUnicodeString) {
-            if (pythonUnicodeString) {
-                var escapedString = pythonUnicodeString.replace(/u'(?=[^:]+')/g, "'");
-                escapedString = escapedString.replace(/'/g,'"');
-                return JSON.parse(escapedString);
-            }
-        }; 
+
 });
 
