9b513e2aa6994ec15d3536758b743d0d0bb162d8
qdonnellan <quentin@burkesoftware.com>
2015-01-09 11:20:24 -0600

    haha so this test was failing because student1 had no grades!! changed it to student2 who had at least 1 grade to test... and it passes

--- ecwsp/grades/tests/test_grade_cache.py
+++ ecwsp/grades/tests/test_grade_cache.py
@@ -2,12 +2,9 @@ from ecwsp.sis.tests import SisTestMixin
 from django.test import TestCase
 
 class GradeCacheTests(SisTestMixin, TestCase):
-    def setUp(self):
-        super(GradeCacheTests, self).setUp()
-        self.build_grade_cache()
 
     def test_passing_letter_grade(self):
-        student = self.data.student
+        student = self.data.student2
         section = self.data.course_section
         grades = student.grade_set.filter(course_section=section)
         for grade in grades:
