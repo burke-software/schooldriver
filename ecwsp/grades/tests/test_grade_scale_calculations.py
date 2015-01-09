086f4643921270e44b865f4ec288e1d9dd4508f4
qdonnellan <quentin@burkesoftware.com>
2015-01-09 14:51:48 -0600

    yes... this works

--- ecwsp/grades/tests/test_grade_scale_calculations.py
+++ ecwsp/grades/tests/test_grade_scale_calculations.py
@@ -3,13 +3,14 @@ from django.test import TestCase
 from ecwsp.sis.models import GradeScaleRule, GradeScale
 from ecwsp.grades.models import Grade
 import time
+from decimal import Decimal
 
 class GradeScaleTests(SisTestMixin, TestCase):
     def setUp(self):
         super(GradeScaleTests, self).setUp()
         self.build_grade_cache()
         scale = self.scale = GradeScale.objects.create(name="test")
-        GradeScaleRule.objects.create(min_grade=50, max_grade=59.99, letter_grade='F', numeric_scale=1, grade_scale=scale)
+        GradeScaleRule.objects.create(min_grade=50, max_grade=Decimal('59.99'), letter_grade='F', numeric_scale=1, grade_scale=scale)
         GradeScaleRule.objects.create(min_grade=60, max_grade=69.99, letter_grade='D', numeric_scale=1.5, grade_scale=scale)
         GradeScaleRule.objects.create(min_grade=70, max_grade=79.99, letter_grade='C', numeric_scale=2, grade_scale=scale)
         GradeScaleRule.objects.create(min_grade=80, max_grade=89.99, letter_grade='B', numeric_scale=3, grade_scale=scale)
@@ -20,7 +21,7 @@ class GradeScaleTests(SisTestMixin, TestCase):
     def test_grade_scale(self):
         scale = self.scale
         self.assertEqual(scale.to_letter(50), 'F')
-        self.assertEqual(scale.to_letter(59.99), 'F')
+        self.assertEqual(scale.to_letter(Decimal('59.99')), 'F')
         self.assertEqual(scale.to_letter(55.34234), 'F')
         self.assertEqual(scale.to_letter(1000), None)
         self.assertEqual(scale.to_numeric(50), 1)
