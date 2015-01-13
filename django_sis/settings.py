607b71f450f86151df48738f01f6e48af4bc8984
David Burke <dmbst32@gmail.com>
2015-01-13 02:38:49 -0500

    Trying to fix s3

--- django_sis/settings.py
+++ django_sis/settings.py
@@ -524,7 +524,7 @@ if USE_S3:
     AWS_PRELOAD_METADATA = True
     AWS_QUERYSTRING_AUTH = False
     DEFAULT_FILE_STORAGE = 'django_sis.s3utils.MediaRootS3BotoStorage'
-    COMPRESS_STORAGE = STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+    COMPRESS_STORAGE = STATICFILES_STORAGE = 'django_sis.s3utils.CachedS3BotoStorage''
     COMPRESS_URL = STATIC_URL = 'https://{}.s3.amazonaws.com/'.format(AWS_STORAGE_BUCKET_NAME)
     MEDIA_URL = STATIC_URL
     # Use Heroku's DB
