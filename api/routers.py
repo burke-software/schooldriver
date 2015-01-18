7609b6678c599676fc7315e2d84a728010ee5195
David Burke <dmbst32@gmail.com>
2015-01-18 13:11:19 -0500

    gradebook api maybe?

--- api/routers.py
+++ api/routers.py
@@ -7,18 +7,21 @@ from api.admissions.views import ApplicationTemplateViewSet
 from api.admissions.views import ApplicantAdditionalInformationViewSet
 from api.admissions.views import EmergencyContactViewSet
 from api.admissions.views import ApplicantForeignKeyRelatedFieldChoicesViewSet
+from ecwsp.gradebook.api_views import AssignmentViewSet
+
 
 router = routers.DefaultRouter()
 router.register(r'grades', GradeViewSet)
 router.register(r'courses', CourseViewSet)
 router.register(r'sections', SectionViewSet)
+router.register(r'assignments', AssignmentViewSet)
 router.register(r'applicant', ApplicantViewSet)
 router.register(r'applicant-custom-field', ApplicantCustomFieldViewSet)
 router.register(r'application-template', ApplicationTemplateViewSet )
 router.register(r'emergency-contact', EmergencyContactViewSet)
 router.register(r'applicant-additional-information', ApplicantAdditionalInformationViewSet )
-router.register(r'applicant-foreign-key-field-choices', 
-    ApplicantForeignKeyRelatedFieldChoicesViewSet, 
+router.register(r'applicant-foreign-key-field-choices',
+    ApplicantForeignKeyRelatedFieldChoicesViewSet,
     base_name='applicant-foreign-key-field-choices'
     )
 api_urls = router.urls
