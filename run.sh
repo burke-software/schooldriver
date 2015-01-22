f84ebf6f8bd5d6470a5a2d9baf0aa220a672215e
qdonnellan <quentin@burkesoftware.com>
2015-01-22 17:12:50 -0600

    removing second worker and restoring the local ./run.sh program

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 export C_FORCE_ROOT="true"
-supervisord -c /etc/supervisor/supervisord.conf
+python manage.py celery worker -E -A django_sis -B --loglevel=INFO &
+python manage.py celery flower &
+supervisord -c /etc/supervisor/conf.d/supervisord.conf
 python manage.py runserver_plus 0.0.0.0:8000
 
 
