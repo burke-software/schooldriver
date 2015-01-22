e2de27261a4b5e6c572d797a208e86f4ad5ee0e5
qdonnellan <quentin@burkesoftware.com>
2015-01-22 16:57:09 -0600

    my best guess at an improved (supervised) celery arsenal on docker...

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 export C_FORCE_ROOT="true"
-python manage.py celery worker -E -A django_sis -B --loglevel=INFO &
-python manage.py celery flower &
 supervisord -c /etc/supervisor/supervisord.conf
 python manage.py runserver_plus 0.0.0.0:8000
 
