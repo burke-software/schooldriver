21fb8bdb0bffa75bdf37349b3ffce2c05cd6a656
qdonnellan <quentin@burkesoftware.com>
2015-01-27 14:58:35 -0600

    initial attempt to get Travis + SauceLabs + Protractor to all play nice...

--- static_files/app/tests/conf.js
+++ static_files/app/tests/conf.js
@@ -1,6 +1,4 @@
-exports.config = {
-  // The address of a running selenium server.
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+var config = {
 
   // The div which contains the app (this should be changed when
   // all angular apps use the SpaView() page)
@@ -12,3 +10,18 @@ exports.config = {
     admissions: ['admissions/student_application_spec.js',],
   },
 };
+
+
+if (process.env.TRAVIS) {
+    config.sauceUser = process.env.SAUCE_USERNAME;
+    config.sauceKey = process.env.SAUCE_ACCESS_KEY;
+    config.capabilities = {
+        'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+        build: process.env.TRAVIS_BUILD_NUMBER
+    };
+} else {
+    config.seleniumAddress: 'http://localhost:4444/wd/hub';
+}
+
+exports.config = config;
+
