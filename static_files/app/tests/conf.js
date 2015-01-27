996ba03139b1c97fc21d9da2595caa479ff044d1
qdonnellan <quentin@burkesoftware.com>
2015-01-27 16:57:02 -0600

    ok, definitely a sign that I probably need to take a break from this for a bit... ... aka typos

--- static_files/app/tests/conf.js
+++ static_files/app/tests/conf.js
@@ -21,9 +21,8 @@ var config = {
 if (process.env.TRAVIS) {
     config.sauceUser = process.env.SAUCE_USERNAME;
     config.sauceKey = process.env.SAUCE_ACCESS_KEY;
-    config.capabilities['tunnel-identifier'] = process.env.TRAVIS_JOB_NUMBER,
-    config.capabilities['build'] = process.env.TRAVIS_BUILD_NUMBER
-    };
+    config.capabilities['tunnel-identifier'] = process.env.TRAVIS_JOB_NUMBER;
+    config.capabilities['build'] = process.env.TRAVIS_BUILD_NUMBER;
 } else {
     config.seleniumAddress = 'http://localhost:4444/wd/hub';
 }
