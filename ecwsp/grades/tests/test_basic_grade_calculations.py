f762676bf0fa7d513617e47399162a6ecdea2ca5
David Burke <dmbst32@gmail.com>
2015-02-13 12:06:00 -0500

    Moving unit tests for better organization

--- ecwsp/grades/tests/test_basic_grade_calculations.py
+++ ecwsp/grades/tests/test_basic_grade_calculations.py
@@ -1,18 +1,54 @@
 from ecwsp.grades.models import Grade
-from ecwsp.sis.models import Student, SchoolYear
 from ecwsp.sis.tests import SisTestMixin
 from django.test import TestCase
 from ecwsp.sis.sample_data import SisData
-from ecwsp.schedule.models import (
-    CourseEnrollment, Course, CourseSection, MarkingPeriod
-    )
+from ecwsp.schedule.models import CourseEnrollment
+from ..models import FinalGrade
 import datetime
+
 
 class GradeCalculationTests(SisTestMixin, TestCase):
     def setUp(self):
         self.data = SisData()
         self.data.create_basics()
-        self.build_grade_cache()
+
+    def get_simple_test_data(self):
+        student = self.data.student
+        mp1 = self.data.marking_period
+        mp2 = self.data.marking_period2
+        mp3 = self.data.marking_period3
+        course = self.data.course_section1
+        return [
+            [student, course, mp1, 100, 100],
+            [student, course, mp2, 50, 75],
+            [student, course, mp3, 90.5, 80.17],
+        ]
+
+    def set_grade(self, enrollment, mp, grade):
+        grade_obj = Grade(enrollment=enrollment, marking_period=mp)
+        grade_obj.set_grade(grade)
+        grade_obj.save()
+
+    def set_final_grade(self, enrollment, grade):
+        grade_obj = FinalGrade(enrollment=enrollment)
+        grade_obj.set_grade(grade)
+        grade_obj.save()
+
+    def check_grade(self, enrollment, expect, date=None):
+        average = Grade.get_course_grade(enrollment, date=date)
+        self.assertEquals(average, expect)
+
+    def set_and_check_grade(self, enrollment, mp, grade, expect,
+                            date=None):
+        self.set_grade(enrollment, mp, grade)
+        self.check_grade(enrollment, expect, date=date)
+
+    def test_basic_grades(self):
+        """ /docs/specs/course_grades.md#Rounding """
+        for data in self.get_simple_test_data():
+            enrollment = CourseEnrollment.objects.get(
+                user=data[0], course_section=data[1])
+            self.set_and_check_grade(enrollment, data[2], data[3], data[4])
 
     def test_course_average_respect_time(self):
         """ See /docs/specs/course_grades.md#Time and averages
@@ -24,84 +60,35 @@ class GradeCalculationTests(SisTestMixin, TestCase):
         student = self.data.student
         mp1 = self.data.marking_period
         mp2 = self.data.marking_period2
+        date1 = datetime.date(2099, 1, 1)
+        date2 = datetime.date(2014, 10, 1)
         section = self.data.course_section1
         enroll = self.data.course_enrollment
-        grade1, created = Grade.objects.get_or_create(
-            student=student,
-            course_section=section,
-            marking_period=mp1,
-            grade=mark1,)
-        grade2, created = Grade.objects.get_or_create(
-            student=student,
-            course_section=section,
-            marking_period=mp2,
-            grade=mark2,)
-        self.assertEquals(
-            enroll.get_grade(date_report=datetime.date(2099, 1, 1)),
-            75)
-        self.assertEquals(
-            enroll.get_grade(date_report=datetime.date(2014, 10, 1)),
-            50)
+        self.set_grade(enroll, mp1, mark1)
+        self.set_grade(enroll, mp2, mark2)
+        self.check_grade(enroll, 75, date=date1)
+        self.check_grade(enroll, 50, date=date2)
 
-    # Test compares two ways of calling calculate_grade()
-    # There shouldn't be a discrepancy
-    def test_current_vs_older(self):
-        self.student = Student(first_name='Billy', last_name='Bob', username='BillyB', id=12345)
-        self.student.save()
-        self.year = SchoolYear(name='2011-2012', start_date='2011-09-10', end_date='2012-06-15', grad_date='2012-06-17',
-                               id=2011)
-        self.year.save()
-        self.mp = MarkingPeriod(name="tri1 2011", start_date='2011-09-10', end_date='2012-06-15', school_year=self.year, monday=True, friday=True)
-        self.mp.save()
-        self.mp2 = MarkingPeriod(name="tri2 2012", start_date='2011-09-10', end_date='2012-06-15', school_year=self.year, monday=True, friday=True)
-        self.mp2.save()
-        self.mp3 = MarkingPeriod(name="tri3 2012", start_date='2011-09-10', end_date='2012-06-15', school_year=self.year, monday=True, friday=True)
-        self.mp3.save()
-        courses = [Course(fullname='Algebra', shortname='alg', id=12, credits=4),
-                   Course(fullname='English', shortname='eng', id=13, credits=4),
-                   Course(fullname='History', shortname='hist', id=14, credits=4)]
-        course_sections = []
-        for course in courses:
-            course.save()
-            course_section = CourseSection(
-                course=course,
-                name=course.fullname
-            )
-            course_section.save()
-            course_sections.append(course_section)
-        course_sections[0].marking_period.add(self.mp)
-        course_sections[1].marking_period.add(self.mp2)
-        course_sections[2].marking_period.add(self.mp3)
-        grades = [
-            Grade(
-                student=self.student,
-                course_section=course_sections[0],
-                grade=86.78,
-                marking_period=self.mp
-            ), Grade(
-                student=self.student,
-                course_section=course_sections[1],
-                grade=94.73,
-                marking_period=self.mp2
-            ), Grade(
-                student=self.student,
-                course_section=course_sections[2],
-                grade=77.55,
-                marking_period=self.mp3
-            )
-        ]
-        for grade in grades:
-            grade.save()
-        course_enrollments = [
-            CourseEnrollment(user=self.student, course_section=course_sections[0], grade=grades[0]),
-            CourseEnrollment(user=self.student, course_section=course_sections[1], grade=grades[1]),
-            CourseEnrollment(user=self.student, course_section=course_sections[2], grade=grades[2])
-        ]
-        for course_enrollment in course_enrollments:
-            course_enrollment.save()
-        StudentYearGrade.objects.create(student=self.student, year=self.year)
-        syg = self.student.studentyeargrade_set.get(year=self.year)
-        current = syg.calculate_grade_and_credits(date_report=datetime.date.today())
-        older = syg.calculate_grade_and_credits()
-        self.assertEqual(current, older)
+    def test_marking_period_weight(self):
+        """ See /docs/specs/course_grades.md#Marking Period Weights """
+        mp1 = self.data.marking_period
+        mp1.weight = 1.5
+        mp1.save()
+        test_data = self.get_simple_test_data()
+        # Adjust expected for mp1 weight of 1.5
+        test_data[1][4] = 80
+        test_data[2][4] = 83
+        for data in test_data:
+            enrollment = CourseEnrollment.objects.get(
+                user=data[0], course_section=data[1])
+            self.set_and_check_grade(enrollment, data[2], data[3], data[4])
 
+    def test_final_override(self):
+        """ See /docs/specs/course_grades.md#Marking Period Weights """
+        test_data = self.get_simple_test_data()
+        for data in test_data:
+            enrollment = CourseEnrollment.objects.get(
+                user=data[0], course_section=data[1])
+            self.set_grade(enrollment, data[2], data[3])
+        self.set_final_grade(enrollment, 82)
+        self.check_grade(enrollment, 82)
