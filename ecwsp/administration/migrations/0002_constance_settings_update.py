101ef1cdae177642fedb438ab8da21a6d1569df0
Christina Levengood <fromageball@gmail.com>
2015-02-19 16:56:36 -0500

    updated migration for constance setting

--- ecwsp/administration/migrations/0002_constance_settings_update.py
+++ ecwsp/administration/migrations/0002_constance_settings_update.py
@@ -7,9 +7,14 @@ from constance import config
 def configuration_to_constance(apps, schema_editor):
     """adds Configuration object data to Constance Config in settings"""
     Configuration = apps.get_model("administration", "Configuration")  
-    configs = Configuration.objects.get(name="school pay rate per hour")
-    formatted_config = configs.name.replace(" ", "_").upper
-    config.formatted_config() = (configs.value, configs.help_text)
+    configs = Configuration.objects.filter(name="school pay rate per hour").first()
+    print configs
+    if configs[0]:
+        formatted_config = configs[0].name.replace(" ", "_").upper
+        formatted = formatted_config()
+        config.name = (configs.value, configs.help_text)
+    else:
+        pass
 
 class Migration(migrations.Migration):
 
