17911f0a58d00ffdf818d47aa37506ba67f1cf03
Brendan <me@brendanberkley.com>
2015-03-24 10:22:18 -0400

    New set of sample data manage.py populate_sample_data --gui to get it; no flag keeps the old set.

--- ecwsp/sis/management/commands/populate_sample_data.py
+++ ecwsp/sis/management/commands/populate_sample_data.py
@@ -2,11 +2,22 @@ from django.core.management.base import BaseCommand, CommandError
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from ecwsp.sis.sample_data import *
+from ecwsp.sis.sample_gui_data import *
 from ecwsp.sis.models import Student
 from ecwsp.sis.management.commands.populate_sample_application import ApplicationTemplateGenerator
+from optparse import make_option
 
 class Command(BaseCommand):
     help = 'Populate blank database'
+    gui = False
+
+    option_list = BaseCommand.option_list + (
+        make_option('--gui',
+            action='store_true',
+            dest='gui',
+            default=False,
+            help='Populate sample data for people to use while demoing or working on front end'),
+    )
 
     def handle(self, *args, **options):
         try:
@@ -18,6 +29,11 @@ class Command(BaseCommand):
                 call_command('syncdb', all=True, interactive=False)
             else:
                 call_command('migrate', interactive=False)
-        SisData().create_all()
-        ApplicationTemplateGenerator().create_default_admission_application()
+
+        if options['gui']:
+            SisGUIData().create_all()
+        else :
+            SisData().create_all()
+            ApplicationTemplateGenerator().create_default_admission_application()
+        
         self.stdout.write('Success. Good Job!')
