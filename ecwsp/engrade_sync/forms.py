89dcdfeb6f127ef4c3d709ff00522d0e831c1177
David Burke <dmbst32@gmail.com>
2015-03-25 10:14:02 -0400

    Fixed engrade sync form

--- ecwsp/engrade_sync/forms.py
+++ ecwsp/engrade_sync/forms.py
@@ -1,15 +1,16 @@
 from django import forms
-from django.forms.widgets import CheckboxSelectMultiple, TextInput
-from django.contrib.auth.models import User
-from localflavor.us.forms import *
 
-from ecwsp.engrade_sync.models import *
 from ecwsp.schedule.models import MarkingPeriod
+from ecwsp.sis.models import Faculty
+
 
 class SetupCourseForm(forms.Form):
     marking_period = forms.ModelChoiceField(MarkingPeriod.objects.all())
-    
+
+
 class GradeSyncForm(forms.Form):
     marking_period = forms.ModelChoiceField(MarkingPeriod.objects.all())
-    teachers = forms.ModelMultipleChoiceField(Faculty.objects.filter(is_active=True,teacher=True))
-    include_comments = forms.BooleanField(required=False,initial=True)
+    teachers = forms.ModelMultipleChoiceField(
+        Faculty.objects.filter(is_active=True, teacher=True))
+    include_comments = forms.BooleanField(
+        required=False, initial=True)
