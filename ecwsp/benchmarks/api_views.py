cb0481902a324d26218c111a2c87f4f3c8787747
Brendan <me@brendanberkley.com>
2015-04-01 11:37:02 -0400

    IsAdminUser for gradebook endpoints

--- ecwsp/benchmarks/api_views.py
+++ ecwsp/benchmarks/api_views.py
@@ -1,8 +1,10 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
 from .serializers import BenchmarkSerializer
 from .models import Benchmark
 
 
 class BenchmarkViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAdminUser,)
     serializer_class = BenchmarkSerializer
     queryset = Benchmark.objects.all()
--- ecwsp/gradebook/api_views.py
+++ ecwsp/gradebook/api_views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
 from .serializers import (
     AssignmentSerializer, MarkSerializer, AssignmentCategorySerializer, 
     AssignmentTypeSerializer)
@@ -6,20 +7,24 @@ from .models import Assignment, Mark, AssignmentCategory, AssignmentType
 
 
 class AssignmentViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAdminUser,)
     serializer_class = AssignmentSerializer
     queryset = Assignment.objects.all()
 
 
 class MarkViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAdminUser,)
     serializer_class = MarkSerializer
     queryset = Mark.objects.all()
 
 
 class AssignmentCategoryViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAdminUser,)
     serializer_class = AssignmentCategorySerializer
     queryset = AssignmentCategory.objects.all()
 
 
 class AssignmentTypeViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAdminUser,)
     serializer_class = AssignmentTypeSerializer
     queryset = AssignmentType.objects.all()
